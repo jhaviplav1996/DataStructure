43e59dc42e0b55253b9ac92e3ee9c81999531fab
jhaviplav1996 <53143552+jhaviplav1996@users.noreply.github.com>
2021-10-16 17:41:57 +0530

    first commit
    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBu3-KAlXW5CSNap10m1B__FgG6zykkDEk",
+    authDomain: "clone-d156e.firebaseapp.com",
+    databaseURL: "https://clone-d156e.firebaseio.com",
+    projectId: "clone-d156e",
+    storageBucket: "clone-d156e.appspot.com",
+    messagingSenderId: "938554084359",
+    appId: "1:938554084359:web:cf443059f32ef9862f6ea2",
+    measurementId: "G-LCYQ6889PY"
+  };
